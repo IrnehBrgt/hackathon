54b54af977c8ed761fbcead398cf600ff749b94c
IrnehBrgt <henri.bergeret@gmail.com>
2022-06-29 15:17:11 +0200

    bouton vers le forum

--- hackathon/src/pages/Homepage.jsx
+++ hackathon/src/pages/Homepage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import "../assets/Homepage.css";
 
 function Homepage() {
   return (
     <div>
       <h1>Hello on INSIDE-ABSIDE ! </h1>
+      <button type='button' className='forum-button'>
+          <NavLink className='details' to='/forum'>
+            Forum
+          </NavLink>
+        </button>
     </div>
   )
 }
