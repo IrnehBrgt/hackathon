7e35712a0beace2078c6bbb65d62b6da9a6c580f
ChristelDeveza <chris.dvz66@gmail.com>
2022-06-29 17:38:32 +0200

    sidebar nav

---
+++ hackathon/src/components/Sidebardata.js
@@ -0,0 +1,32 @@
+export const Sidebardata=[
+  {
+    title:"Home",
+    path: "/",
+    cName: "nav-text"
+  },
+  {
+    title:"Mon compte",
+    path: "/moncompte",
+    cName: "nav-text"
+  },
+  {
+    title:"A la une chez Apsite",
+    path:"/actu",
+    cName: "nav-text"
+  },
+  {
+    title:"Plus d'actualités",
+    path:"/plusdactu",
+    cName: "nav-text"
+  },
+  {
+    title:"La communauté des Apsidiens",
+    path:"/forum",
+    cName: "nav-text"
+  },
+  {
+    title:"Les agences",
+    path:"/agences",
+    cName: "nav-text"
+  }
+]
--- hackathon/src/pages/Homepage.jsx
+++ hackathon/src/pages/Homepage.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import "../assets/Homepage.css";
+import Sidebar from '../components/Sidebar';
 
 function Homepage() {
   return (
     <div>
+      <Sidebar />
       <h1>Welcome on INSIDE-ABSIDE ! </h1>
+      
       <button type='button' className='forum-button'>
           <NavLink className='details' to='/forum'>
             Forum
           </NavLink>
+          
+
         </button>
+
+
     </div>
   )
 }
