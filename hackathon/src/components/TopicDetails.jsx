e87b638e712223a33530568d30a8ca0e5b9981d8
IrnehBrgt <henri.bergeret@gmail.com>
2022-06-30 09:16:07 +0200

    1

--- hackathon/src/components/TopicDetails.jsx
+++ hackathon/src/components/TopicDetails.jsx
@@ -4,12 +4,12 @@ import ContentOfTopic from './ContentOfTopic';
 function TopicDetails() {
   return (
     <div className="Comments">
-      <div>
+      <div className='1'>
         <h3>{ContentOfTopic[0].title}</h3>
         <p>{ContentOfTopic[0].text}</p>
         <p>{ContentOfTopic[0].Profil}</p>
         </div>
-        <div>
+        <div className='2'>
         <p>{ContentOfTopic[0].comment[0].OneComment}</p>
         <p>{ContentOfTopic[0].comment[0].Profil}</p>
         </div>
@@ -17,7 +17,7 @@ function TopicDetails() {
         <p>{ContentOfTopic[0].comment[1].OneComment}</p>
         <p>{ContentOfTopic[0].comment[1].Profil}</p>
         </div>
-        <div>
+        <div className='3'>
         <p>{ContentOfTopic[0].comment[2].OneComment}</p>
         <p>{ContentOfTopic[0].comment[2].Profil}</p>
         </div>
