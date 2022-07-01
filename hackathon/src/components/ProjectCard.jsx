b47b38a952a7dbdfe7e595dade029bbd5af966fa
saif-eddine-miah <saifeddinemiah@gmail.com>
2022-07-01 10:45:29 +0200

    kjngr

--- hackathon/src/components/ProjectCard.jsx
+++ hackathon/src/components/ProjectCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BsTag } from 'react-icons/bs';
+import { FaUserCircle } from 'react-icons/fa';
 
 import '../assets/ProjectCard.css';
 
@@ -12,7 +14,10 @@ function ProjectCard(props) {
       <div className='Txt'>
         <h3 className='ProjectTxt'>status: En cours</h3>
         <h3 className='ProjectTxt'>Participants</h3>
+        <p><FaUserCircle /><FaUserCircle /><FaUserCircle /><FaUserCircle /><FaUserCircle /><FaUserCircle /><FaUserCircle /></p>
         <h3 className='ProjectTxt'>Categories</h3>
+        <button> <BsTag /> Automobile</button>
+        <button> <BsTag /> Apside Toulouse</button>
       </div>
     </div>
   );
--- hackathon/src/pages/Renault.jsx
+++ hackathon/src/pages/Renault.jsx
@@ -1,7 +1,8 @@
 import { React } from 'react';
-import ProjectCard from '../components/ProjectCard';
 import image from '../photos_et_logos/renault.jpeg';
 import Sidebar from '../components/Sidebar';
+import { BsTag, BsChat } from 'react-icons/bs';
+import { FaUserCircle } from 'react-icons/fa';
 import '../assets/Renault.css'
 
 function Renault() {
@@ -10,15 +11,21 @@ function Renault() {
       <div className='sidebar'>
         <Sidebar />
       </div>
-      <div className='body'>
-      <ProjectCard
-        title={'Renault'}
-        descriptions={
-          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque nam vel eum dolorum illo nihil impedit at! Rerum, officiis facilis hic mollitia assumenda itaque consectetur dolore ducimus quae ut exercitationem quasi corrupti explicabo similique nulla ad iste error vitae veritatis consequatur fugiat impedit deleniti pariatur aperiam? Deserunt consequatur optio animi!'
-        }
-        image={image}
-      />
+      <div className='ProjectCard'>
+      <img src={image} alt='coucou' />
+      <h1>{'Renault'}</h1>
+      <p className='ProjectTxt'>{  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque nam vel eum dolorum illo nihil impedit at! Rerum, officiis facilis hic mollitia assumenda itaque consectetur dolore ducimus quae ut exercitationem quasi corrupti explicabo similique nulla ad iste error vitae veritatis consequatur fugiat impedit deleniti pariatur aperiam? Deserunt consequatur optio animi!'
+}</p>
+      <div className='Txt'>
+        <h3 className='ProjectTxt'>status: En cours</h3>
+        <h3 className='ProjectTxt'>Participants</h3>
+        <p><FaUserCircle /><FaUserCircle /><FaUserCircle /><FaUserCircle /><FaUserCircle /><FaUserCircle /><FaUserCircle /></p>
+        <h3 className='ProjectTxt'>Categories</h3>
+        <button> <BsTag /> Automobile</button>
+        <button> <BsTag /> Apside Toulouse</button>
       </div>
+      <button className='Contact'> <BsChat /> Contact</button>
+    </div>
     </div>
   );
 }
